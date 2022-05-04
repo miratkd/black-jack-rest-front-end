f6eb57b30bad44fb7aeafe1cdca3d253865d79b0
lucas mira <lucas-mira1@hotmail.com>
2022-05-03 21:06:41 -0300

    free ticket button

--- src/views/MyAccount.vue
+++ src/views/MyAccount.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>
-    test
+  <div class="page-containear">
+    <div class="account-section-containear">
+      <div class="account-section-line">{{this.$store.state.account.user.username}}</div>
+      <div class="account-section-line">{{this.$store.state.account.user.email}}</div>
+      <div class="account-section-line">Tickets: {{this.$store.state.account.tickets}}</div>
+    </div>
+    <div class="actions-section-containear">
+      <div class="tickets-section-containear">
+        <div class="daily-ticket">Resgatar ticket diario!</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,3 +26,42 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.page-containear{
+  display: flex;
+  min-height: 86.3vh;
+}
+.account-section-containear{
+  background-color: lightseagreen;
+  margin: 30px;
+  width: 20vw;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: white;
+  font-size: 25px;
+  padding: 30px 0;
+}
+.account-section-line{
+  margin-bottom: 30px;
+}
+.tickets-section-containear{
+  display: flex;
+  justify-content: flex-end;
+}
+.actions-section-containear{
+  flex: 1;
+  margin: 30px;
+}
+.daily-ticket{
+  background-color: lightseagreen;
+  color: white;
+  padding: 25px;
+  border-radius: 10px;
+  font-size: 25px;
+  cursor: pointer;
+  border: 3px solid lightsalmon;
+}
+</style>
