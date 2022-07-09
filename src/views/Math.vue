d351bdab3d6c1c5d8e70561ace365a784283bcf4
lucas mira <lucas-mira1@hotmail.com>
2022-07-09 18:39:41 -0300

    back button

--- src/views/Math.vue
+++ src/views/Math.vue
@@ -5,15 +5,18 @@
       <div class="math-page-header-button">Round: {{math.math_active_round}}</div>
     </div>
     <div class="math-page-body">
-      <div class="math-page-body-score-container">
-        <div>
-          <div class="math-page-body-score-text math-page-body-score-name">{{$store.state.account.user.username}}</div>
-          <div class="math-page-body-score-text">{{math.rounds_won}}</div>
+      <div>
+        <div class="math-page-body-score-container">
+          <div>
+            <div :title="$store.state.account.user.username" class="math-page-body-score-text math-page-body-score-name">{{formatName($store.state.account.user.username)}}</div>
+            <div class="math-page-body-score-text math-page-body-score-value">{{math.rounds_won}}</div>
+          </div>
+          <div>
+            <div class="math-page-body-score-text math-page-body-score-name">Casa</div>
+            <div class="math-page-body-score-text math-page-body-score-value">{{getHouseRounds(math)}}</div>
+          </div>
         </div>
-        <div>
-          <div class="math-page-body-score-text math-page-body-score-name">Casa</div>
-          <div class="math-page-body-score-text">{{getHouseRounds(math)}}</div>
-        </div>
+        <div class="math-page-body-back">Voltar</div>
       </div>
       <div>test</div>
     </div>
@@ -29,6 +32,10 @@ export default {
     }
   },
   methods: {
+    formatName (name) {
+      if (name.length > 9) return name.slice(0, 9) + '...'
+      return name
+    },
     getMath () {
       const url = this.$store.state.backEndUrl + 'math/' + localStorage.getItem('activeMath') + '/'
       const config = { headers: { Authorization: this.$store.state.accessToken } }
@@ -81,7 +88,7 @@ export default {
   color: white;
   padding: 2vw;
   border-radius: 10px;
-  font-size: 2.5em;
+  font-size: 2.2em;
   border: 3px solid lightsalmon;
   width: 25vw;
   display: flex;
@@ -92,4 +99,17 @@ export default {
   text-transform: uppercase;
 }
 .math-page-body-score-name{border-bottom: 2px solid white;}
+.math-page-body-score-value{font-size: 2em;}
+.math-page-body-back{
+  background-color: lightseagreen;
+  color: white;
+  padding: 2vw 0;
+  border-radius: 10px;
+  font-size: 2.5em;
+  border: 3px solid lightsalmon;
+  text-align: center;
+  width: 50%;
+  cursor: pointer;
+  margin-top: 3vh;
+}
 </style>
